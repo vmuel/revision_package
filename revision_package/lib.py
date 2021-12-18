63b5a1096ad25ba28049c83677165caa0d7eb303
vincent muel <vincem@gmx.fr>
2021-12-18 17:47:40 +0100

    Initial Push

---
+++ revision_package/lib.py
@@ -0,0 +1,9 @@
+def try_me()
+    print('hello!')
+    print('what is the capital city of liechtenstein')
+    answer = input()
+    if answer == 'vaduz'
+        print('congrats! good asnwer')
+    else:
+        print('WRONG')
+        print('You loose LOOSER')
